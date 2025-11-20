8f86717e686a5fc8f28d643a4183793239c4d2a0
sani816 <saniakhatun622@gmail.com>
2025-11-20 13:29:36 +0530

    Day1_q17

---
+++ Module_2/JS_Assighment/DAY1_js_q17.js
@@ -0,0 +1,33 @@
+//A.
+
+arr1 = [1, 2, 3];
+arr2 = [4, 5];
+console.log([...arr1,...arr2])
+
+//B.
+
+function sum(...nums) {
+    return nums.reduce((total, num) => total + num, 0);
+}
+console.log(sum(1,2,3,4,5))
+
+//C.
+
+const user = {
+    name: "Alice",
+    age: 22,
+    address: {
+      city: "Bangalore",
+      pin: 560001
+    }
+  };
+
+
+const { 
+    name, 
+    address: { city, pin } 
+  } = user;
+  
+  console.log(name);
+  console.log(city);
+  console.log(pin);
