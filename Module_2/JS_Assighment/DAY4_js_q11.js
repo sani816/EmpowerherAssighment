8136ca1e83338a6d8f4f3b9c0b2d66bb132c88e2
sani816 <saniakhatun622@gmail.com>
2025-11-22 13:13:59 +0530

    Replace setTimeout with immediate console logs

--- Module_2/JS_Assighment/DAY4_js_q11.js
+++ Module_2/JS_Assighment/DAY4_js_q11.js
@@ -1,3 +1,3 @@
-setTimeout(()=>{console.log("Task 1")},1000)
-setTimeout(()=>{console.log("Task 2")},1500)
-setTimeout(()=>{console.log("Task 3")},2000)
+console.log("Task 1")
+console.log("Task 2")
+console.log("Task 3")
